23f8fe029e1eb45f3bca3417b006929e16cc8d55
Artem66 <melodpi@gmail.com>
2020-04-08 15:09:20 +0300

    Updates

---
+++ precache-manifest.c3ce44519941003e3709d02f72ca07c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100545bb5222cf55371058abbe601ef4",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "2f16853244f729e48209",
+    "url": "/social-network/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "bf6b55dd33b34f686478",
+    "url": "/social-network/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2f16853244f729e48209",
+    "url": "/social-network/static/js/2.2d68e19a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/social-network/static/js/2.2d68e19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6b55dd33b34f686478",
+    "url": "/social-network/static/js/main.d4daa21f.chunk.js"
+  },
+  {
+    "revision": "f23d3fb05ab6b682feb1",
+    "url": "/social-network/static/js/runtime-main.f6cfeeaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
